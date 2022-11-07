9a027b11c91aed1557560aeb0dcd82776ae0853f
bryan klein <bklein3@cnm.edu>
2022-11-07 14:39:43 -0700

    updated files

--- sql/food-truck-project.sql
+++ sql/food-truck-project.sql
@@ -3,67 +3,61 @@
 -- this is akin to reformatting and reinstalling Windows (OS X never needs a reinstall...) ;)
 -- never ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever ever
 -- do this on live data!!!!
-DROP TABLE IF EXISTS `like`;
-DROP TABLE IF EXISTS image;
-DROP TABLE IF EXISTS tweet;
-DROP TABLE IF EXISTS `profile`;
+DROP TABLE IF EXISTS menu;
+DROP TABLE IF EXISTS truck;
+DROP TABLE IF EXISTS customer;
+DROP TABLE IF EXISTS owner;
 
-CREATE TABLE profile (
-    -- this creates the attribute for the primary key
-    -- auto_increment tells mySQL to number them {1, 2, 3, ...}
-    -- not null means the attribute is required!
-                         profileId BINARY(16) NOT NULL,
-                         profileActivationToken CHAR(32),
-                         profileAtHandle VARCHAR(32) NOT NULL UNIQUE,
-                         profileAvatarUrl  VARCHAR(255),
-    -- to make sure duplicate data cannot exist, create a unique index
-                         profileEmail VARCHAR(128) NOT NULL,
-    -- to make something optional, exclude the not null
+CREATE TABLE owner (
+
+                         owner_id UUID NOT NULL, PRIMARY KEY,
+                         owner_name varchar(64) NOT NULL,
+                         owner_phone VARCHAR(32) NOT NULL UNIQUE,
+                         owner_email  VARCHAR(64) NOT NULL UNIQUE ,
+                         owner_hash VARCHAR(64) NOT NULL,
                          profileHash CHAR(97) NOT NULL,
-                         profilePhone VARCHAR(32),
-                         UNIQUE(profileEmail),
-                         UNIQUE(profileAtHandle),
-    -- this officiates the primary key for the entity
-                         PRIMARY KEY(profileId)
+                         profile_phone VARCHAR(32),
+                         owner_activation_token CHAR(32) NOT NULL
 );
--- create the tweet entity
-CREATE TABLE tweet (
-    -- this is for yet another primary key...
-                       tweetId BINARY(16) NOT NULL,
-    -- this is for a foreign key; auto_incremented is omitted by design
-                       tweetProfileId BINARY(16) NOT NULL,
-                       tweetContent VARCHAR(140) NOT NULL,
-    -- notice dates don't need a size parameter
-                       tweetDate DATETIME(6) NOT NULL,
-    -- this creates an index before making a foreign key
-                       INDEX(tweetProfileId),
-    -- this creates the actual foreign key relation
-                       FOREIGN KEY(tweetProfileId) REFERENCES profile(profileId),
-    -- and finally create the primary key
-                       PRIMARY KEY(tweetId)
+CREATE TABLE Customer (
+                       customer_id UUID NOT NULL PRIMARY,
+                       customer_name VARCHAR(20) NOT NULL UNIQUE ,
+                       customer_phone VARCHAR(32) NOT NULL,
+                       customer_hash CHAR(97) NOT NULL,
+                       customer_email VARCHAR(64) NOT NULL  ,
+                       customer_activation_token VARCHAR(32) NULL,
 );
--- create the tweetImage entity
-CREATE TABLE image (
-                       imageId BINARY(16) NOT NULL,
-                       imageTweetId BINARY(16) NOT NULL,
-                       imageCloudinaryToken VARCHAR(255) NOT NULL,
-                       imageUrl VARCHAR(128) NOT NULL ,
-                       INDEX(imageId),
-                       INDEX(imageTweetId),
-                       FOREIGN KEY(imageTweetId) REFERENCES tweet(tweetId),
-                       PRIMARY KEY (imageId)
+CREATE TABLE truck (
+                       truck_id UUID NOT NULL PRIMARY KEY,
+                       truck_owner_id UUID NOT NULL,
+                       truck_card_accepted VARCHAR(3) NOT NULL,
+                       food_truck_type VARCHAR(32) NOT NULL ,
+                       truck_lat DECIMAL(8,5),
+                       truck_lan DECIMAL(8,5),
+                       FOREIGN KEY (truck_owner_id) REFERENCES owner(owner_id),
 );
--- create the like entity (a weak entity from an m-to-n for profile --> tweet)
-CREATE TABLE `like` (
-    -- these are not auto_increment because they're still foreign keys
-                        likeTweetId BINARY(16) NOT NULL,
-                        likeProfileId BINARY(16) NOT NULL,
-                        likeDate DATETIME(6) NOT NULL,	-- index the foreign keys
-                        INDEX(likeProfileId),
-                        INDEX(likeTweetId),
-    -- create the foreign key relations
-                        FOREIGN KEY(likeTweetId) REFERENCES tweet(tweetId),
-                        FOREIGN KEY(likeProfileId) REFERENCES profile(profileId),
-    -- finally, create a composite foreign key with the two foreign keys
-                        PRIMARY KEY(likeProfileId, likeTweetId)
+CREATE INDEX ON truck(truck_ower_id);
+
+
+CREATE TABLE menu (
+                        menu_id UUID NOT NULL,
+                        menu_truck_id UUID NOT NULL,
+                        menu_name VARCHAR(128) NOT NULL,
+                        menu_price VARCHAR(5) NOT NULL ,
+                        menu_description VARCHAR(256) ,
+                        menu_img_url VARCHAR(512),
+                        FOREIGN KEY (menu_truck_id) REFERENCES truck   (truck_id)
+ );
+CREATE INDEX ON menu    (menu_truck_id)
+
+CREATE TABLE Favorite(
+    favorite_truck_id    BINARY(16) UUID NOT NULL,
+    favorite_customer_id BINARY(16) UUID NOT NULL,
+ PRIMARY KEY (favorite_truck_id, favorite_customer_id),
+    FOREIGN KEY (favorite_customer_id) REFERENCES truck (truck_id),
+    FOREIGN KEY (favorite_truck_id) REFERENCES Customer (customer_id)
 );
+CREATE INDEX ON favorites (favorite_truck_id);
+CREATE INDEX ON favorites (favorite_customer_id);
+
+
