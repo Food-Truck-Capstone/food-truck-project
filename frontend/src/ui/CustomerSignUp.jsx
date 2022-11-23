dc00c3e5bd07a96e20a618043562a3ec15e13b47
Josh Foley <jfoley5@cnm.edu>
2022-11-23 09:59:03 -0700

    working on sign up

---
+++ frontend/src/ui/CustomerSignUp.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {Container, Row} from "react-bootstrap";
+
+export function CustomerSignUp() {
+    return (
+        <>
+            <Container>
+                <form id="signUpForm">
+                    <label>
+                        <Row id="SignUpText">
+                            Username:
+                            <input type="text" name="name"/>
+                        </Row>
+                        <Row id="SignUpText">
+                            Password:
+                            <input type="password" name="password"/>
+                        </Row>
+                        <Row id="SignUpText">
+                            Confirm Password:
+                            <input type="password" name="password"/>
+                        </Row>
+                        <Row id="SignUpText">
+                            Phone:
+                            <input type="text" name="phone"/>
+                        </Row>
+                        <Row id="SignUpText">
+                            Email:
+                            <input type="text" name="email"/>
+                        </Row>
+                    </label>
+                    <Row>
+                        <input id="submitButton" type="submit" value="Submit"/>
+                    </Row>
+                </form>
+            </Container>
+        </>
+    )
+}
--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import {CustomerSignUp} from "./CustomerSignUp";
+import {OwnerSignUp} from "./OwnerSignUp";
 
 export function Home() {
     return (
         <>
             <h1>Home</h1>
+            <CustomerSignUp/>
+            <OwnerSignUp/>
         </>
     )
 }
