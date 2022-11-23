f0f4d20ac992ae350c6e030f10ce7b869fcb3477
Michael Perea <mperea104@cnm.edu>
2022-11-23 11:41:25 -0700

    Menu Item created

--- frontend/src/ui/Home.jsx
+++ frontend/src/ui/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import {MenuItem} from "./MenuItem.jsx";
 
 export function Home() {
     return (
         <>
             <h1>Home</h1>
+            <MenuItem/>
         </>
     )
 }
---
+++ frontend/src/ui/MenuItem.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import {Col, Container, Row} from "react-bootstrap";
+
+export function MenuItem() {
+    return (
+        <Container className={"border border-dark rounded"}>
+            <Row>
+                <Col md={1} className={"text-center my-auto order-sm-last"}>
+                    <img src={"https://placekitten.com/60/60"} className={"rounded-circle"}/>
+                </Col>
+
+                <Col md={3}>
+                    <h5 className={"d-inline"}>Item Name</h5>
+                    <h5 className={"d-inline"}> - </h5>
+                    <h5 className={"d-inline"}>Item Price</h5>
+                    <p> Item Description Lorem Ipsum One of the best ways to describe food on your menu is by indicating how it was prepared. So long as your customer recognizes the words you choose, it will give them a clear picture of your food's flavor and appearance.</p>
+                </Col>
+
+            </Row>
+        </Container>
+    )
+}
