811cdb9fe2f2691df2bc3da8c13714f2a346e90f
Josh Foley <jfoley5@cnm.edu>
2022-11-26 12:56:28 -0700

    sign up pages

--- frontend/src/ui/OwnerSignUp.jsx
+++ frontend/src/ui/OwnerSignUp.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import {Container, Row} from "react-bootstrap";
-import Switch from "react-switch";
 
-export function OwnerSignUp(){
-    return(
+
+export function OwnerSignUp() {
+    return (
         <>
             <Container>
                 <form id="signUpForm">
                     <label>
                         <Row id="SignUpText">
-                            Truck Brand:
+                            Truck Name:
                             <input type="text" name="name"/>
+                        </Row>
+                        <Row id="SignUpText">
+                            Owner Name:
+                            <input type="text" name="name"/>
+                        </Row>
+                        <Row className="mt-5">
+                            Cash ONLY?
+                            <label className="switch">
+                                <input type="checkbox"></input>
+                                <span className="slider round"></span>
+                            </label>
+
                         </Row>
                         <Row id="SignUpText">
                             Type of Food:
@@ -19,7 +31,10 @@ export function OwnerSignUp(){
                                 <option value="option 2">Option 2</option>
                                 <option value="option 3">Option 3</option>
                             </select>
-
+                        </Row>
+                        <Row id="SignUpText">
+                            Truck Number (for multiple trucks under same name) :
+                            <input type="text" name="truck number"/>
                         </Row>
                         <Row id="SignUpText">
                             Password:
@@ -33,16 +48,17 @@ export function OwnerSignUp(){
                             Phone:
                             <input type="text" name="phone"/>
                         </Row>
-                        <Row id="SignUpText">
+                        <Row id="SignUpText" className="mb-5">
                             Email:
                             <input type="text" name="email"/>
                         </Row>
                     </label>
                     <Row>
-                        <input id="submitButton" type="submit" value="Submit"/>
+                        <input id="ownerSubmitButton" type="submit" value="Submit"/>
                     </Row>
                 </form>
             </Container>
+
         </>
     )
 }
